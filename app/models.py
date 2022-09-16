79490ec870628a88ec10a57e27ed604b256d699b
ssulthanah <sulthanahsp@gmail.com>
2022-09-16 17:42:54 +0700

    konsul1

---
+++ app/models.py
@@ -0,0 +1,43 @@
+from tkinter import CASCADE
+from django.db import models
+
+# Create your models here.
+class obat(models.Model):
+    idobat = models.CharField(max_length=10, primary_key=True)
+    jenisobat = models.CharField(max_length=50)
+    namaobat = models.CharField(max_length=10)
+    jumlahstock = models.IntegerField()
+    hargabeli = models.IntegerField()
+    hargajual = models.IntegerField()
+    satuan = models.IntegerField()
+
+class penjualan(models.Model):
+    idpenjualan = models.CharField(max_length=10, primary_key=True)
+    namaapoteker = models.CharField(max_length=50)
+    namapelanggan = models.CharField(max_length=50)
+    tanggaltransaksi = models.DateField()
+
+class supplier(models.Model):
+    idsupplier = models.CharField(max_length=10, primary_key=True)
+    namasupplier = models.CharField(max_length=50)
+    alamat = models.CharField(max_length=50)
+    nohp = models.IntegerField()
+
+class pembelian(models.Model):
+    idpembelian = models.CharField(max_length=10, primary_key=True)
+    idsupplier = models.ForeignKey(supplier, on_delete=models.CASCADE)
+    namaapoteker = models.CharField(max_length=50)
+    tanggalpembelian = models.DateField()
+
+class detailpenjualan(models.Model):
+    iddetailpenjualan = models.CharField(max_length=10, primary_key=True)
+    idpenjualan = models.ForeignKey(penjualan, on_delete=models.CASCADE)
+    idobat = models.ForeignKey(obat, on_delete=models.CASCADE)
+    jumlahobatterjual = models.IntegerField()
+
+class detailpembelian(models.Model):
+    iddetailpembelian = models.CharField(max_length=10, primary_key=True)
+    idpembelian = models.ForeignKey(pembelian, on_delete=models.CASCADE)
+    idobat = models.ForeignKey(obat, on_delete=models.CASCADE)
+    jumlahobatdibeli = models.IntegerField()
+
---
+++ app/urls.py
@@ -0,0 +1,7 @@
+from ssl import VERIFY_ALLOW_PROXY_CERTS
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    
+]
