da068fd3a94ecd2aaf0db82653d864d721a55242
ssulthanah <sulthanahsp@gmail.com>
2022-09-16 19:04:22 +0700

    konsul1

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.obat)
+admin.site.register(models.penjualan)
+admin.site.register(models.supplier)
+admin.site.register(models.pembelian)
+admin.site.register(models.detailpenjualan)
+admin.site.register(models.detailpembelian)
--- app/models.py
+++ app/models.py
@@ -3,41 +3,58 @@ from django.db import models
 
 # Create your models here.
 class obat(models.Model):
-    idobat = models.CharField(max_length=10, primary_key=True)
+    idobat = models.CharField(max_length=5, primary_key=True)
     jenisobat = models.CharField(max_length=50)
     namaobat = models.CharField(max_length=10)
     jumlahstock = models.IntegerField()
     hargabeli = models.IntegerField()
     hargajual = models.IntegerField()
-    satuan = models.IntegerField()
+    satuan = models.CharField()
+
+    def __str__(self):
+        return str(self.namaobat)
 
 class penjualan(models.Model):
-    idpenjualan = models.CharField(max_length=10, primary_key=True)
+    idpenjualan = models.CharField(max_length=5, primary_key=True)
     namaapoteker = models.CharField(max_length=50)
     namapelanggan = models.CharField(max_length=50)
     tanggaltransaksi = models.DateField()
 
+    def __str__(self):
+        return str(self.namapelanggan)
+
 class supplier(models.Model):
-    idsupplier = models.CharField(max_length=10, primary_key=True)
+    idsupplier = models.CharField(max_length=5, primary_key=True)
     namasupplier = models.CharField(max_length=50)
     alamat = models.CharField(max_length=50)
     nohp = models.IntegerField()
 
+    def __str__(self):
+        return str(self.namasupplier)
+
 class pembelian(models.Model):
-    idpembelian = models.CharField(max_length=10, primary_key=True)
+    idpembelian = models.CharField(max_length=5, primary_key=True)
     idsupplier = models.ForeignKey(supplier, on_delete=models.CASCADE)
     namaapoteker = models.CharField(max_length=50)
     tanggalpembelian = models.DateField()
 
+    def __str__(self):
+        return str(self.idsupplier)
+
 class detailpenjualan(models.Model):
-    iddetailpenjualan = models.CharField(max_length=10, primary_key=True)
+    iddetailpenjualan = models.CharField(max_length=5, primary_key=True)
     idpenjualan = models.ForeignKey(penjualan, on_delete=models.CASCADE)
     idobat = models.ForeignKey(obat, on_delete=models.CASCADE)
     jumlahobatterjual = models.IntegerField()
 
+    def __str__(self):
+        return str(self.idpenjualan)
+
 class detailpembelian(models.Model):
-    iddetailpembelian = models.CharField(max_length=10, primary_key=True)
+    iddetailpembelian = models.CharField(max_length=5, primary_key=True)
     idpembelian = models.ForeignKey(pembelian, on_delete=models.CASCADE)
     idobat = models.ForeignKey(obat, on_delete=models.CASCADE)
     jumlahobatdibeli = models.IntegerField()
 
+    def __str__(self):
+        return str(self.idpembelian)
