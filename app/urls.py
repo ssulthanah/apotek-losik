f0b26a79f490a5e858480de256a1ed82ffc0f628
ssulthanah <sulthanahsp@gmail.com>
2022-09-25 20:10:04 +0700

    konsul1

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,7 @@
-from ssl import VERIFY_ALLOW_PROXY_CERTS
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    
+    path('obat', views.obat,name='obat'),
+    path('addobat',views.addobat,name='addobat')    
 ]
--- app/views.py
+++ app/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from datetime import datetime
+import re
+from django.shortcuts import render, redirect
+from . import models
+
 
 # Create your views here.
+
+def obat(request):
+    allobatobj = models.obat.objects.all()
+    getobatobj = models.obat.objects.get(idobat=1)
+    
+    return render (request, 'obat.html',{
+    'allobatobj' : allobatobj,
+    'getobatobj' : getobatobj
+    })
+    
+def addobat (request) :
+    if request.method == "GET" :
+        return render(request, 'addobat.html')
+    else:
+        jenisobat = request.POST['jenisobat']
+        namaobat = request.POST['namaobat']
+        jumlahstock = request.POST['jumlahstock']
+        hargabeli = request.POST['hargabeli']
+        hargajual = request.POST['hargajual']
+        satuan = request.POST['satuan']
+
+        newobat = models.obat(
+            jenisobat = jenisobat,
+            namaobat = namaobat,
+            jumlahstock = jumlahstock,
+            hargabeli = hargabeli,
+            hargajual = hargajual,
+            satuan = satuan,
+        ).save()
+        return redirect('obat')
